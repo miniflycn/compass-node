1c41fcf4cd924048dc7ed7b38a33e83decb0a8ee
Daniel Yang <miniflycn@justany.net>
2014-08-10 11:41:47 +0800

    add sprite analyse

--- test/scss.js
+++ test/scss.js
@@ -5,15 +5,30 @@ var Scss = require('../lib/scss')
   , compareLine = require('./lib/compareLine');
 
 describe('scss', function () {
-  it('should xxx', function () {
-    compareLine(
-      new Scss(
-        fs.readFileSync(path.join(__dirname, './scss/import.scss'), { encoding: 'utf8' }), 
-        {
-          includePaths: [path.join(__dirname, './scss')]
-        }
-      ).getContext(),
-      './test/scss/reset.scss'
-    );
+  describe('import', function () {
+    it('should xxx', function () {
+      compareLine(
+        new Scss(
+          fs.readFileSync(path.join(__dirname, './scss/import.scss'), { encoding: 'utf8' }), 
+          {
+            includePaths: [path.join(__dirname, './scss')]
+          }
+        ).getContext(),
+        './test/scss/reset.scss'
+      );
+    });
   });
+
+  // describe('sprite', function () {
+  //   it('should xxx', function () {
+  //     console.log(
+  //       new Scss(
+  //         fs.readFileSync(path.join(__dirname, './scss/sprites.scss'), { encoding: 'utf8' }), 
+  //         {
+  //           includePaths: [path.join(__dirname, './scss')]
+  //         }
+  //       ).getContext()
+  //     );
+  //   });
+  // });
 });
