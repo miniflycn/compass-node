4f71b4d5d882f131108ddd2152a463606574b673
Daniel Yang <miniflycn@justany.net>
2014-08-14 23:52:37 +0800

    add scss prefix engine's test cases

--- test/lib/compareLine.js
+++ test/lib/compareLine.js
@@ -2,7 +2,7 @@
 var fs = require('fs');
 
 function compareLine(css, file) {
-  css = css.replace(/\r?\n$/, '').split('\n');
+  css = css.replace(/(\r?\n)+$/, '').split('\n');
   file = fs.readFileSync(file, { encoding: 'utf8' }).replace(/\r?\n$/, '').split(/\r?\n/);
   css.forEach(function (line, i) {
     line.should.equal(file[i]);
--- test/scss.js
+++ test/scss.js
@@ -8,14 +8,61 @@ describe('scss', function () {
   describe('import', function () {
     it('should xxx', function (done) {
       new Scss(
-        fs.readFileSync(path.join(__dirname, './scss/import.scss'), { encoding: 'utf8' }), 
+        fs.readFileSync(path.join(__dirname, './engine/import.scss'), { encoding: 'utf8' }), 
         {
-          includePaths: [path.join(__dirname, './scss')]
+          includePaths: [path.join(__dirname, './engine')]
         }
       ).done(function (context) {
-        compareLine(context, './test/scss/reset.scss');
+        compareLine(context, './test/engine/import-should.scss');
         done();
       });
     });
   });
+
+  describe('custom function', function () {
+    it('should xxx', function (done) {
+      new Scss(
+        fs.readFileSync(path.join(__dirname, './engine/custom-function.scss'), { encoding: 'utf8' }),
+        {
+          includePaths: [path.join(__dirname, './engine')],
+          functions: {
+            'test': function (args) {
+              return '"hello"';
+            },
+            'display-block': function (args) {
+              var arg = args[0];
+              return arg + '-block';
+            }
+          }
+        }
+      ).done(function (context) {
+        compareLine(context, './test/engine/custom-function-should.scss');
+        done();
+      })
+    });
+  });
+
+  describe('mixin', function () {
+    var mixins = require('../lib/mixins');
+    // my custom mixin
+    mixins['font-mixin'] = function (args, done) {
+      var weight = args[0]
+        , size = args[1]
+        , lineHeight = args[2]
+        , family = args[3];
+      done('font: ' + weight + ' ' + size + '/' + lineHeight + ' ' + family);
+    }
+
+    it('should xxx', function (done) {
+      new Scss(
+        fs.readFileSync(path.join(__dirname, './engine/mixin.scss'), { encoding: 'utf8' }),
+        {
+          includePaths: [path.join(__dirname, './engine')]
+        }
+      ).done(function (context) {
+        compareLine(context, './test/engine/mixin-should.scss');
+        done();
+      })
+    });
+  })
 });
